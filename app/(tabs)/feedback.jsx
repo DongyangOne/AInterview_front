75df2dffa85bb724621f96c2fe9729e5e41ed577
takjinwu <105762293+takjinwu@users.noreply.github.com>
2025-07-12 20:50:05 +0900

    Update feedback.jsx

--- app/(tabs)/feedback.jsx
+++ app/(tabs)/feedback.jsx
@@ -1,5 +1,4 @@
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import Feedback_detail from '../Feedback_detail';
 import Feedback_result from '../Feedback_result';
 
 
@@ -10,7 +9,6 @@ export default function Feedback() {
       <Text style={styles.title}>피드백 페이지</Text>
       <View style={styles.separator} />
        <SafeAreaView style={{ flex: 1 }}>
-            <Feedback_detail />
             <Feedback_result />
           </SafeAreaView>
     </View>
