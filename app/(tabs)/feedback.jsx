5f762c1c3f348e7155f01a7146bc5654a804a81f
takjinwu <takjinwu0613@gmail.com>
2025-07-12 20:27:18 +0900

    publuish-19

--- app/(tabs)/feedback.jsx
+++ app/(tabs)/feedback.jsx
@@ -1,10 +1,16 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import Feedback_detail from '../Feedback_detail';
+
+
 
 export default function Feedback() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>피드백 페이지</Text>
       <View style={styles.separator} />
+       <SafeAreaView style={{ flex: 1 }}>
+            <Feedback_detail />
+          </SafeAreaView>
     </View>
   );
 }
--- app/(tabs)/myPage.jsx
+++ app/(tabs)/myPage.jsx
@@ -1,27 +1,111 @@
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, SafeAreaView} from "react-native";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 export default function MyPage() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>마이페이지</Text>
-      <View style={styles.separator} />
+  <SafeAreaView style={styles.container}>
+
+
+        {/*프로필 섹션 */}
+    <View style={styles.profileSection}>
+        <Ionicons name="person-circle-outline" size={100} color="#bbb" />
+        <TouchableOpacity style={styles.editIcon}>
+            <MaterialIcons name="edit" size={16} color="fff" />
+        </TouchableOpacity>
     </View>
+
+    {/*내 정보 관리 */}
+    <View style={styles.section}>
+        <Text style={styles.sectionTitle}>내 정보 관리</Text>
+        <View style={styles.divider} />
+
+        <View style={styles.row}>
+            <Text style={styles.label}>아이디</Text>
+            <Text style={styles.value}>dana1234</Text>
+        </View>
+
+        <TouchableOpacity style={styles.row}>
+            <Text style={styles.label}>닉네임</Text>
+            <View style={styles.right}>
+                <Text style={styles.value}>김다나</Text>
+                <Ionicons name="chevron-forward" size={18} color="#aaa" />
+            </View>
+        </TouchableOpacity>
+        </View>
+
+        {/*계정 관리 */}
+        <View style={styles.section}>
+            <Text style={styles.sectionTitle}>계정 관리</Text>
+            <View style={styles.divider} />
+
+            <TouchableOpacity style={styles.row}>
+                <Text style={styles.label}>로그아웃</Text>
+                <Ionicons name="chevron-forward" size={18} color="#aaa" />
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.row}>
+                <Text style={styles.label}>회원탈퇴</Text>
+                <Ionicons name="chevron-forward" size={18} color="#aaa" />
+            </TouchableOpacity>
+        </View>
+  </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    paddingTop: 40,
+    paddingHorizontal: 20,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
+  profileSection: {
+    alignItems: 'center',
+    marginBottom: 30,
   },
-  separator: {
-    marginVertical: 30,
+  editIcon: {
+    position: 'absolute',
+    bottom: 5,
+    right: 110,
+    backgroundColor: 'purple',
+    borderRadius: 12,
+    padding: 4,
+  },
+  section: {
+    marginBottom: 30,
+  },
+  sectionTitle: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 10,
+    color: '#000',
+  },
+  divider: {
     height: 1,
-    width: "80%",
+    backgroundColor: '#ccc',
+    marginBottom: 10,
+  },
+  row: {
+    paddingVertical: 12,
+    borderBottomWidth: 0.5,
+    borderColor: '#ddd',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  label: {
+    fontSize: 14,
+    color: '#333',
+  },
+  value: {
+    fontSize: 14,
+    color: '#888',
+  },
+  right: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 4,
   },
 });
