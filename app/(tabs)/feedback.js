2796d0578b253648363a22128bdcc61dc213aea3
picnic88 <ssemilife@gmail.com>
2025-07-18 17:06:42 +0900

    publish-8-12

---
+++ app/(tabs)/feedback.js
@@ -0,0 +1,453 @@
+import {
+  View, Text, TextInput, StyleSheet, TouchableOpacity, Alert, ScrollView, Image, Modal,
+} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import useWindowDimensions from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import DropDownPicker from 'react-native-dropdown-picker';
+
+export default function App() {
+
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("basic");
+  const [items, setItems] = useState([
+    { label: '정렬 기준', value: 'basic' },
+    { label: '최근 날짜 순', value: 'latest' },
+    { label: '가나다 순', value: 'name' },
+  ]);
+
+  const [open1, setOpen1] = useState(false);
+  const [open2, setOpen2] = useState(false);
+  const [value1, setValue1] = useState('basic');
+  const [items1, setItems1] = useState([
+    { label: '정렬 기준', value: 'basic' },
+    { label: '최상단 고정', value: 'static' },
+    { label: '제목 수정', value: 'title' },
+    { label: '메모 수정', value: 'memo' },
+    { label: '기록 삭제', value: 'delete' },
+  ]);
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalType, setModalType] = useState(null); // 'title', 'memo', 'delete'
+
+
+
+  return (
+    <View style={{ flex: 1, }}>
+      <View style={styles.head}>
+        <Text style={{ fontSize: 20, top: 10, fontWeight: 'bold' }}>나의 피드백 목록</Text>
+        <View style={styles.search}>
+          <TextInput style={styles.searchInput} placeholder='제목, 날짜, 메모 검색' />
+          <Image
+            source={require('./assets/images/publish8_12_images/search.png')}
+            style={{ width: 25, height: 25, marginLeft: 300, top: -5 }}
+          />
+        </View>
+        <View style={{ top: 60, width: '85%', flexDirection: 'row', }}>
+          <Text style={{ fontSize: 15, marginRight: 140, top: 5, color: '#808080' }}>모든 피드백</Text>
+          <DropDownPicker
+            open={open}
+            value={value}
+            items={items}
+            setOpen={setOpen}
+            setValue={setValue}
+            setItems={setItems}
+            style={{
+              top: -12,
+              width: 130,
+              height: 40,
+              borderWidth: 'none',
+              zIndex: 2000
+            }}
+            dropDownContainerStyle={{
+              fontSize: 14,
+              width: 115,
+              maxHeight: 80,
+            }}
+            textStyle={{ fontSize: 15, }}
+          />
+        </View>
+      </View>
+
+
+      <ScrollView style={[styles.entire, { position: 'relative', }]}>
+
+        //1
+
+        <View style={[styles.contentBox, { position: 'relative' }]}>
+          <Image
+            source={require('./assets/images/publish8_12_images/bookmark.png')}
+            style={{ width: 34, height: 34, marginLeft: 300, top: -17, zIndex: 10, position: 'absolute' }}
+          />
+          <View style={{ flexDirection: 'row', }}>
+            <Text style={styles.fontTw1}>2025.07.06</Text>
+            <TouchableOpacity style={{ top: 20, right: 14 }} onPress={() => { setOpen(false); setModalVisible(true) }}>
+              <Text style={{ marginLeft: 240 }}>•</Text>
+              <Text style={{ marginLeft: 240, top: -10 }}>•</Text>
+              <Text style={{ marginLeft: 240, top: -20 }}>•</Text>
+            </TouchableOpacity>
+            {open1 && (
+              <DropDownPicker
+                open={open1}
+                value={value1}
+                items={items1}
+                setOpen={setOpen1}
+                setValue={setValue1}
+                setItems={setItems1}
+                style={styles.dropdown}
+                zIndex={3000}
+                zIndexInverse={1000}
+                dropDownContainerStyle={styles.dropdownContainer}
+                onChangeValue={(val) => {
+                  console.log("선택된 항목:", val);
+                  setValue1(val);
+                  setOpen1(false);
+                  if (val === 'title') {
+                    setModalVisible(true);
+                  }
+                }}
+              />
+            )}
+          </View>
+          <View style={{ top: -15 }}>
+            <Text style={styles.fontTw2}>삼성 회사 면접</Text>
+            <Text style={styles.fontTw3}>&lt 앞으로 자신감...</Text>
+          </View>
+        </View>
+
+//2
+        <View style={styles.contentBox}>
+          <View style={{ flexDirection: 'row', }}>
+            <Text style={styles.fontTw1}>2025.07.06</Text>
+            <TouchableOpacity onPress={() => { setOpen(false); setOpen1(!open1) }} style={{ top: 20, right: 14 }}>
+              <Text style={{ marginLeft: 240 }}>•</Text>
+              <Text style={{ marginLeft: 240, top: -10 }}>•</Text>
+              <Text style={{ marginLeft: 240, top: -20 }}>•</Text>
+            </TouchableOpacity>
+            {open1 && (
+              <DropDownPicker
+                open={open1}
+                value={value1}
+                items={items1}
+                setOpen={setOpen1}
+                setValue={setValue1}
+                setItems={setItems1}
+                style={styles.dropdown}
+                zIndex={3000}
+                zIndexInverse={1000}
+                dropDownContainerStyle={styles.dropdownContainer}
+                onChangeValue={(val) => {
+                  console.log("선택된 항목:", val);
+                  setValue1(val);
+                  setOpen1(false);
+                  if (val === 'title') {
+                    setModalVisible(true);
+                  }
+                }}
+              />
+            )}
+
+
+          </View>
+          <View style={{ top: -15 }}>
+            <Text style={styles.fontTw2}>삼성 회사 면접</Text>
+            <Text style={styles.fontTw3}>&lt 앞으로 자신감...</Text>
+          </View>
+        </View>
+
+
+//3
+        <View style={styles.contentBox}>
+          <View style={{ flexDirection: 'row', }}>
+            <Text style={styles.fontTw1}>2025.07.06</Text>
+            <TouchableOpacity style={{ top: 20, right: 14 }} onPress={() => { setOpen(false); setModalVisible(true) }}>
+              <Text style={{ marginLeft: 240 }}>•</Text>
+              <Text style={{ marginLeft: 240, top: -10 }}>•</Text>
+              <Text style={{ marginLeft: 240, top: -20 }}>•</Text>
+            </TouchableOpacity>
+            {open1 && (
+              <DropDownPicker
+                open={open1}
+                value={value1}
+                items={items1}
+                setOpen={setOpen1}
+                setValue={setValue1}
+                setItems={setItems1}
+                style={styles.dropdown}
+                zIndex={3000}
+                zIndexInverse={1000}
+                dropDownContainerStyle={styles.dropdownContainer}
+                onChangeValue={(val) => {
+                  console.log("선택된 항목:", val);
+                  setValue1(val);
+                  setOpen1(false);
+                  if (val === 'title') {
+                    setModalVisible(true);
+                  }
+                }}
+              />
+            )}
+          </View>
+          <View style={{ top: -15 }}>
+            <Text style={styles.fontTw2}>삼성 회사 면접</Text>
+            <Text style={styles.fontTw3}>&lt 앞으로 자신감...</Text>
+          </View>
+        </View>
+
+
+//4
+        <View style={styles.contentBox}>
+          <View style={{ flexDirection: 'row', }}>
+            <Text style={styles.fontTw1}>2025.07.06</Text>
+            <TouchableOpacity style={{ top: 20, right: 14 }} onPress={() => { setOpen(false); setModalVisible(true) }}>
+              <Text style={{ marginLeft: 240 }}>•</Text>
+              <Text style={{ marginLeft: 240, top: -10 }}>•</Text>
+              <Text style={{ marginLeft: 240, top: -20 }}>•</Text>
+            </TouchableOpacity>
+            {open1 && (
+              <DropDownPicker
+                open={open1}
+                value={value1}
+                items={items1}
+                setOpen={setOpen1}
+                setValue={setValue1}
+                setItems={setItems1}
+                style={styles.dropdown}
+                zIndex={3000}
+                zIndexInverse={1000}
+                dropDownContainerStyle={styles.dropdownContainer}
+                onChangeValue={(val) => {
+                  console.log("선택된 항목:", val);
+                  setValue1(val);
+                  setOpen1(false);
+                  if (val === 'title') {
+                    setModalVisible(true);
+                  }
+                }}
+              />
+            )}
+          </View>
+          <View style={{ top: -15 }}>
+            <Text style={styles.fontTw2}>삼성 회사 면접</Text>
+            <Text style={styles.fontTw3}>&lt 앞으로 자신감...</Text>
+          </View>
+        </View>
+
+
+//5
+        <View style={styles.contentBox}>
+          <View style={{ flexDirection: 'row', }}>
+            <Text style={styles.fontTw1}>2025.07.06</Text>
+            <TouchableOpacity style={{ top: 20, right: 14 }} onPress={() => { setOpen(false); setModalVisible(true) }}>
+              <Text style={{ marginLeft: 240 }}>•</Text>
+              <Text style={{ marginLeft: 240, top: -10 }}>•</Text>
+              <Text style={{ marginLeft: 240, top: -20 }}>•</Text>
+            </TouchableOpacity>
+            {open1 && (
+              <DropDownPicker
+                open={open1}
+                value={value1}
+                items={items1}
+                setOpen={setOpen1}
+                setValue={setValue1}
+                setItems={setItems1}
+                style={styles.dropdown}
+                zIndex={3000}
+                zIndexInverse={1000}
+                dropDownContainerStyle={styles.dropdownContainer}
+                onChangeValue={(val) => {
+                  console.log("선택된 항목:", val);
+                  setValue1(val);
+                  setOpen1(false);
+                  if (val === 'title') {
+                    setModalVisible(true);
+                  }
+                }}
+              />
+            )}
+          </View>
+          <View style={{ top: -15 }}>
+            <Text style={styles.fontTw2}>삼성 회사 면접</Text>
+            <Text style={styles.fontTw3}>&lt 앞으로 자신감...</Text>
+          </View>
+        </View>
+
+        <View style={{ height: 150 }} />
+      </ScrollView>
+
+
+
+
+      <View style={styles.foot}>
+        <TouchableOpacity style={styles.footBtn1L}>
+          <Image
+            source={require('./assets/images/publish8_12_images/Home.png')}
+            style={{ width: 25, height: 35, }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.footBtn1}>
+          <Image
+            source={require('./assets/images/publish8_12_images/feedback.png')}
+            style={{ width: 25, height: 35, }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.footBtn1}>
+          <View style={{
+            width: 64, height: 64, borderColor: '#5900FF',
+            borderWidth: 3, borderRadius: 50,
+            top: -30, backgroundColor: 'white',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}>
+            <Text style={{ fontWeight: 600, fontSize: 50, color: '#5900FF', top: -5 }}>+</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.footBtn1}>
+          <Image
+            source={require('./assets/images/publish8_12_images/cal.png')}
+            style={{ width: 25, height: 35, }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.footBtn1R}>
+          <Image
+            source={require('./assets/images/publish8_12_images/myPage.png')}
+            style={{ width: 45, height: 35, right: 10, }}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  entire: {
+    alignItems: 'center',
+    width: '100%',
+    height: "100%",
+    top: 130,
+    paddingTop: 20
+  },
+  head: {
+    marginTop: 70,
+    height: 60,
+    alignItems: 'center',
+  },
+  search: {
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#cccccc',
+    top: 40,
+    width: 350,
+    height: 50,
+  },
+  searchInput: {
+    marginLeft: 20,
+    top: 15,
+    width: 280,
+    fontSize: 15
+  },
+
+
+  //--------------content-----------------
+
+  contentBox: {
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: '#cccccc',
+    width: 350,
+    height: 135,
+    marginBottom: 20,
+    zIndex: 3
+  },
+  fontTw1: {
+    marginTop: 26,
+    marginLeft: 18,
+    fontSize: 14,
+    color: '#808080'
+  },
+  fontTw2: {
+    fontSize: 18,
+    marginTop: 23,
+    marginLeft: 18,
+    marginBottom: 10,
+    fontWeight: 'bold'
+  },
+  fontTw3: {
+    fontSize: 16,
+    marginTop: 10,
+    marginLeft: 18,
+    marginBottom: 10,
+    color: '#808080'
+  },
+
+
+
+
+
+
+  //----------------footer------------------
+  foot: {
+    textAlign: 'center',
+    width: '100%',
+    flexDirection: 'row',
+    paddingTop: 0,
+    paddingBottom: 0,
+  },
+  footBtn1: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 10,
+    height: 70,
+    backgroundColor: 'white',
+    borderColor: 'black',
+  },
+  footBtn1L: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 10,
+    height: 70,
+    borderTopLeftRadius: 20,
+    backgroundColor: 'white',
+    borderColor: 'black',
+  },
+  footBtn1R: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 10,
+    height: 70,
+    borderTopRightRadius: 20,
+    backgroundColor: 'white',
+    borderColor: 'black',
+  },
+
+
+
+
+  dropdown: {
+    top: 30,
+    right: 30,
+    width: 120,
+    maxHeight: 50,
+    zIndex: 1000,
+    backgroundColor: 'white'
+  },
+  dropdownContainer: {
+    position: 'absolute',
+    fontSize: 14,
+    width: 160,
+    maxHeight: 195,
+    right: 350,
+    zIndex: 1000,
+    backgroundColor: 'white'
+  },
+
+  modalBtn: {
+    width: 140,
+    height: 45,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+
+});
