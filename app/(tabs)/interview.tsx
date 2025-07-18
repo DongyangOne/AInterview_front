7ce4653e1e2d53683dff701c23811b03ba2b6e38
PJW <pyojaewon03@gmail.com>
2025-07-19 03:03:57 +0900

    publish-16

--- app/(tabs)/interview.tsx
+++ app/(tabs)/interview.tsx
@@ -1,27 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from 'react';
+import { View, Button } from 'react-native';
+import CustomModal from '../../components/Modal/Modal';
 
 export default function Interview() {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>면접 페이지</Text>
-      <View style={styles.separator} />
+    <View style={{ flex: 1 }}>
+      <Button title="모달 열기" onPress={() => setModalVisible(true)} />
+      <CustomModal
+        visible={modalVisible}
+        onCancel={() => setModalVisible(false)}
+        onConfirm={() => {
+          // 종료 처리 로직
+          setModalVisible(false);
+        }}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
