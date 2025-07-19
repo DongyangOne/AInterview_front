abd8ae3b64bb0f7012770b7d08cc2b186170ce73
PJW <pyojaewon03@gmail.com>
2025-07-20 04:46:02 +0900

    publish-18

--- app/Interview_result.js
+++ app/Interview_result.js
@@ -1,14 +1,40 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image } from 'react-native';
+import { useRouter } from 'expo-router';
+import CustomModal from '../components/Modal/Close';
 
 export default function Interview_result() {
+  const router = useRouter();
+  const [modalVisible, setModalVisible] = useState(false);
   const [title, setTitle] = useState('');
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.closeButton}>
-        <Text style={styles.closeText}>×</Text>
-      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.closeButton}
+        onPress={() => setModalVisible(true)}
+        >
+            <View>
+                <Image
+                source={require("../assets/icons/close.png")}
+                style={{
+                top: 23,
+                left: 20,
+                width: 17,
+                height: 17,
+                }}
+                resizeMode="contain"
+                />
+            </View>
+        </TouchableOpacity>
+        <CustomModal
+          visible={modalVisible}
+          onCancel={() => setModalVisible(false)}
+          onConfirm={() => {
+            // 종료 처리 로직
+            setModalVisible(false);
+          }}
+        />
       <Text style={styles.header}>처리가 완료되었습니다.</Text>
       <Text style={styles.label}>면접 제목을 입력해주세요.</Text>
       <TextInput
@@ -35,25 +61,22 @@ const styles = StyleSheet.create({
   closeButton: {
     position: 'absolute',
     top: 50,
-    left: 30,
+    left: 20,
     zIndex: 2,
   },
-  closeText: {
-    fontSize: 32,
-    color: '#333',
-  },
   header: {
-    fontSize: 24,
+    fontSize: 28,
     fontWeight: '700',
     fontFamily: 'Pretendard',
-    color: '#171717',
+    color: '#191919',
     marginLeft: 8,
     marginBottom: 30,
     textAlign: 'left',
   },
   label: {
-    fontSize: 16,
+    fontSize: 18,
     fontFamily: 'Pretendard',
+    fontWeight: 500,
     color: '#171717',
     marginLeft: 8,
     marginBottom: 10,
@@ -77,15 +100,21 @@ const styles = StyleSheet.create({
     width: 348,
     marginLeft: 8,
     marginTop: 50,
+    marginBottom: 140,
     borderRadius: 10,
     backgroundColor: '#5900FF',
     justifyContent: 'center',
     alignItems: 'center',
+    shadowColor: "#000000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 7,
   },
   saveButtonText: {
     color: '#fff',
-    fontSize: 16,
-    fontWeight: '700',
+    fontSize: 20,
+    fontWeight: '600',
     fontFamily: 'Pretendard',
   },
 });
