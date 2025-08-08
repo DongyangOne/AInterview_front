92ff71c30a8e7d858a61502e474fddab4e76009b
kimbosung521 <sorisl37@naver.com>
2025-08-08 18:28:06 +0900

    오늘의 질문
    오늘의 질문 api로 질문 받아와 화면에 띄움

--- components/main/MainQuestion.js
+++ components/main/MainQuestion.js
@@ -1,11 +1,55 @@
 import { Text, View, StyleSheet } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function MainQuestion() {
+  const [questToday, setQuestToday] = useState("");
+
+  useEffect(() => {
+    const fetchQuestion = async () => {
+      const today = new Date().toLocaleDateString();
+      try {
+        const lastDate = await AsyncStorage.getItem("lastDate");
+        const TodayQuest = await AsyncStorage.getItem("questToday");
+
+        console.log("lastDate:", lastDate);
+        console.log("TodayQuest:", TodayQuest);
+
+        if (lastDate !== today) {
+          // 날짜 다르면 새로운 질문 요청
+          const res = await axios.get(
+            "http://183.101.17.181:3001/question/today"
+          );
+          const question = res.data.data.question;
+
+          console.log("새 질문 받아옴:", question);
+
+          setQuestToday(question);
+          await AsyncStorage.setItem("lastDate", today);
+          await AsyncStorage.setItem("questToday", question);
+        } else {
+          // 날짜 같으면 저장된 질문 사용
+          if (TodayQuest) {
+            console.log("저장된 질문 사용");
+            setQuestToday(TodayQuest);
+          } else {
+            console.log("저장된 질문 없음");
+          }
+        }
+      } catch (err) {
+        console.error("오류발생", err);
+      }
+    };
+
+    fetchQuestion();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.questionTitle}>오늘의 질문</Text>
       <Text style={styles.questionContent}>
-        가장 최선을 다했던 경험은 무엇인가요?
+        {questToday ? questToday : "로딩 중..."}
       </Text>
     </View>
   );
