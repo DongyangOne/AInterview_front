df503cae8ad63c2ae649979cd508cceed29bc25e
kimbosung521 <sorisl37@naver.com>
2025-08-09 17:16:42 +0900

    front-4 메인>금주 일정 조회

--- components/main/MainCalendar.js
+++ components/main/MainCalendar.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // 이번 주(일~토) 날짜 배열 반환
 function getCurrentWeekDates() {
@@ -14,18 +16,75 @@ function getCurrentWeekDates() {
   });
 }
 
-// 날짜별 일정 데이터 예시
-const schedules = {
-  0: "회의",
-  2: "면접",
-  4: "프로젝트",
-  5: "프로젝트",
-};
-
 // 주간 달력 컴포넌트
 function WeeklyCalendar() {
   const weekDates = getCurrentWeekDates();
   const today = new Date();
+  const [schedules, setSchedules] = useState({});
+
+  useEffect(() => {
+    axios
+      .get("http://183.101.17.181:3001/calendar/thisweek", {
+        params: { userId: 1 },
+      })
+      .then((res) => {
+        const scheduleMap = {};
+        const weekStart = new Date(weekDates[0]);
+        weekStart.setHours(0, 0, 0, 0); // 자정 시작
+
+        const weekEnd = new Date(weekDates[6]);
+        weekEnd.setHours(23, 59, 59, 999); // 하루 끝
+        //가져온 값의 길이를 스토리지에 저장
+        const getcount = res.data.data.length;
+        //문자열로 변환
+        AsyncStorage.setItem("weekSchedules", getcount.toString());
+
+        console.log("갯수", getcount);
+        console.log("0", weekStart.toLocaleString("ko-KR"));
+
+        console.log("6", weekEnd);
+        res.data.data.forEach((item) => {
+          const date = new Date(item.time);
+          console.log(
+            "받아온 시간 한국시간 버전",
+            date.toLocaleDateString("ko-KR", {
+              year: "numeric",
+              month: "2-digit",
+              day: "2-digit",
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit",
+              hour12: false,
+            })
+          );
+
+          // 이번 주 범위 체크
+          if (
+            //이번주 시작 시간보다 크고  이번주 마지막 시간보다 작을 때 작동
+            date.getTime() >= weekStart.getTime() &&
+            date.getTime() <= weekEnd.getTime()
+          ) {
+            console.log("시작", weekStart.getTime());
+            console.log("마지막", weekEnd.getTime());
+            //2025.8.8 14:00
+            console.log("밀리초", date.getTime());
+
+            //요일 숫자로 반환
+            const dayIndex = date.getDay();
+            //해당 날짜 배열 없을 시 생성
+            if (!scheduleMap[dayIndex]) {
+              scheduleMap[dayIndex] = [];
+            }
+            //배열에 값 추가
+            scheduleMap[dayIndex].push(item.title);
+          }
+        });
+        setSchedules(scheduleMap);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <View style={styles.calendarContainer}>
@@ -46,20 +105,26 @@ function WeeklyCalendar() {
             {isToday ? (
               <>
                 <View style={styles.todayCircle}>
-                  {isToday && scheduleText && <View style={styles.todayDot} />}
+                  {scheduleText && <View style={styles.todayDot} />}
                   <Text style={styles.todayDateText}>{date.getDate()}</Text>
                 </View>
-                {scheduleText && (
-                  <Text style={styles.scheduleText}>{scheduleText}</Text>
-                )}
+                {scheduleText &&
+                  scheduleText.map((t, i) => (
+                    <Text key={i} style={styles.scheduleText}>
+                      {t}
+                    </Text>
+                  ))}
               </>
             ) : (
               <>
                 {scheduleText && <View style={styles.dot} />}
                 <Text style={styles.normalDateText}>{date.getDate()}</Text>
-                {scheduleText && (
-                  <Text style={styles.scheduleText}>{scheduleText}</Text>
-                )}
+                {scheduleText &&
+                  scheduleText.map((t, i) => (
+                    <Text key={i} style={styles.scheduleText}>
+                      {t}
+                    </Text>
+                  ))}
               </>
             )}
           </TouchableOpacity>
