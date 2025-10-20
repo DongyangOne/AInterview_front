196af415c6a0a736569fafbaec49e743633ec094
chldPDms <115327796+chldPDms@users.noreply.github.com>
2025-10-20 23:40:58 +0900

    면접 카테고리 넓이 수정

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -327,7 +327,7 @@ const styles = StyleSheet.create({
     position: "absolute",
     left: 0,
     right: 0,
-    top: height * 0.525,
+    top: height * 0.625,
     alignItems: "center",
     zIndex: 12,
   },
